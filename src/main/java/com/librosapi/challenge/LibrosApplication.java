c721af47c4cfc67fd25ec7e8204d1a7a2e068ad5
Rodrigo Ezequiel <rodrigocubilla87@gmail.com>
2024-05-20 18:24:42 -0300

    mi primer commit

---
+++ src/main/java/com/librosapi/challenge/LibrosApplication.java
@@ -0,0 +1,13 @@
+package com.librosapi.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/librosapi/challenge/LibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librosapi.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
